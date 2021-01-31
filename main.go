11bd659a6f67b20a450f3f4f29e8b63cf75ba7d4
Denis Karpenko <new.den84@gmail.com>
2021-01-31 14:52:19 +0200

    add method facebook graph api /me

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 
 func main()  {
 	social := socials.NewFacebook();
-	social.SetToken("fb");
-	println(social.GetToken())
+	social.SetToken("EAAXNr3F3gqYBAKJ0w27NKJJZAVxUXKZBTztpxTExQRLXno9088BifiBYe7OjJnWuiZAToFjosZBXZAZCfnbjl9WDAN5XENqneUUx8AiqAaFzIKyGeetZAC8ZAAMkH2SGWVl9epZCa6BWhrE7kEVQWZAUyyozEQl1WfGl7egL7wZCrJup1wl5vA3MC64lhxMzRyjZBkgU09gzGhpM0wZDZD");
+	meInfo,status := social.GetMe()
+	if status {
+		println(meInfo)
+	} else {
+		println(social.GetToken())
+	}
 	//slice := []int{ 31, 72, 3, 14, 15, 6, 27, 18, 59, 10 }
 	//sort := sort.NewSort(slice)
 	//sort.SortSelect()
--- socials/facebook.go
+++ socials/facebook.go
@@ -1,5 +1,13 @@
 package socials
 
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const FACEBOOK_GRAPH_URL = "https://graph.facebook.com/v9.0/"
+
 type Facebook struct {
 	token string
 }
@@ -10,6 +18,34 @@ func (f *Facebook) SetToken(token string) {
 
 func (f *Facebook) GetToken() string{
 	return  f.token;
+}
+
+func (f *Facebook) GetMe() (string, bool) {
+	if f.token == "" {
+		return "", false
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", FACEBOOK_GRAPH_URL + "me/", nil)
+	if err != nil {
+		return "Bad request", false
+	}
+
+	q := req.URL.Query()
+	q.Add("access_token", f.GetToken())
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "Errored when sending request to the server", false
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(body),true
 }
 
 func NewFacebook() *Facebook {
